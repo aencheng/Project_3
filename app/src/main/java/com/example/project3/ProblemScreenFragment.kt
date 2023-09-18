efc573da37e4f00ee1816354c0a8fd26ae01eb41
aencheng <111991942+aencheng@users.noreply.github.com>
2023-09-18 03:43:27 -0400

    Fixed the UI and layout of the screens.

--- app/src/main/java/com/example/project3/ProblemScreenFragment.kt
+++ app/src/main/java/com/example/project3/ProblemScreenFragment.kt
@@ -2,6 +2,7 @@ package com.example.project3
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,8 +10,8 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 
 class ProblemScreenFragment : Fragment() {
     @SuppressLint("MissingInflatedId")
@@ -22,19 +23,36 @@ class ProblemScreenFragment : Fragment() {
 
         val startButton = view.findViewById<Button>(R.id.startButton)
         val difficultyRadioGroup = view.findViewById<RadioGroup>(R.id.difficultyRGroup)
-        var difficultySelectText = ""
+        val selectionRadioGroup = view.findViewById<RadioGroup>(R.id.selectionRGroup)
+        val questionNumbers = view.findViewById<TextView>(R.id.questionNumber)
+
+        var difficultySelection = ""
+        var operandSelection = ""
+        var numberOfQuestions = questionNumbers.text
 
         difficultyRadioGroup.setOnCheckedChangeListener{
-            group, checkedId -> val radioButton = view.findViewById<RadioButton>(checkedId)
-            difficultySelectText = radioButton.text.toString()
+                _, checkedId -> val radioButton = view.findViewById<RadioButton>(checkedId)
+            difficultySelection = radioButton.text.toString()
         }
 
+        selectionRadioGroup.setOnCheckedChangeListener{
+                _, checkedId -> val radioButton = view.findViewById<RadioButton>(checkedId)
+            operandSelection = radioButton.text.toString()
+        }
 
         startButton.setOnClickListener{
-            //findNavController().navigate(R.id.action_problemScreenFragment_to_problemSolvingScreen)
-            val action = ProblemScreenFragmentDirections.actionProblemScreenFragmentToProblemSolvingScreen(difficultySelectText)
+            if(difficultySelection != "" && operandSelection != "" && numberOfQuestions.toString().toInt() != 0){
+//                val combinedSelection = "$difficultySelection|$operandSelection"
+//                val action = ProblemScreenFragmentDirections.actionProblemScreenFragmentToProblemSolvingScreen(combinedSelection)
+//                Log.e("Check Variable", combinedSelection)
+//                view.findNavController().navigate(action)
 
-            view.findNavController().navigate(action)
+                val combinedSelection = "$difficultySelection|$operandSelection|$numberOfQuestions"
+                Log.e("Check Variable", combinedSelection)
+                val action = ProblemScreenFragmentDirections
+                    .actionProblemScreenFragmentToProblemSolvingScreen(combinedSelection)
+                view.findNavController().navigate(action)
+            }
         }
 
         return view
--- app/src/main/java/com/example/project3/ProblemSolvingScreen.kt
+++ app/src/main/java/com/example/project3/ProblemSolvingScreen.kt
@@ -8,28 +8,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import org.w3c.dom.Text
 
 class ProblemSolvingScreen : Fragment() {
     private var param1: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString("difficultySelection")
-            Log.e("CLASS",param1!!)
+        arguments?.getString("combinedSelection")?.let {
+            param1 = it
+            Log.e("CLASS", param1!!)
         }
     }
 
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("MissingInflatedId", "SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_problem_solving_screen, container, false)
         val text = view.findViewById<TextView>(R.id.testText)
-        text.text = param1
+        val list = param1!!.split("|")
+        val difficulty = list[0]
+        val operand = list[1]
+        val numberOfQuestions = list[2]
+        text.text = "$difficulty - $operand - $numberOfQuestions"
         return view
     }
 }
